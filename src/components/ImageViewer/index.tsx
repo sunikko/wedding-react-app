7ff5316c7ff0e87cbda7f511d75621181f38bfbb
soy <soy@soyui-MacBookAir.local>
2025-11-03 03:11:33 +0900

    feat: add modal image viewer with close button and selected image control
    Implemented open/close and selected image logic in ImageGallery. Added CloseButton

--- src/components/ImageViewer/index.tsx
+++ src/components/ImageViewer/index.tsx
@@ -1,15 +1,38 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
-import styles from './ImageViewer.module.scss'
 import classNames from 'classnames/bind'
+
+import styles from './ImageViewer.module.scss'
 import './swiper.css'
+
+import CloseIcon from '@assets/icons/close.svg'
 
 const cx = classNames.bind(styles)
 
-function ImageViewer({ images }: { images: string[] }) {
+function ImageViewer({
+  images,
+  open = false,
+  selectedIdx,
+  onClose,
+}: {
+  images: string[]
+  open: boolean
+  selectedIdx: number
+  onClose: () => void
+}) {
+  if (open === false) {
+    return null
+  }
+
   return (
     <div className={cx('dimmed')}>
-      <Swiper spaceBetween={20} slidesPerView={1} loop={true}>
+      <CloseButton className={cx('icon-close')} onClose={onClose} />
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        loop={true}
+        initialSlide={selectedIdx}
+      >
         {images.map((src, idx) => {
           return (
             <SwiperSlide key={idx}>
@@ -19,6 +42,18 @@ function ImageViewer({ images }: { images: string[] }) {
         })}
       </Swiper>
     </div>
+  )
+}
+
+function CloseButton({
+  onClose,
+  className,
+}: {
+  onClose: () => void
+  className?: string
+}) {
+  return (
+    <img src={CloseIcon} className={className} onClick={onClose} alt="Close" />
   )
 }
 export default ImageViewer
--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,4 +1,5 @@
 import classname from 'classnames/bind'
+import { useState } from 'react'
 import styles from './ImageGallery.module.scss'
 import Section from '@shared/Section'
 import ImageViewer from '@components/ImageViewer'
@@ -6,18 +7,40 @@ import ImageViewer from '@components/ImageViewer'
 const cx = classname.bind(styles)
 
 function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIdx, setSelectedIdx] = useState(-1)
+
+  const open = selectedIdx > -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
   return (
     <>
       <Section title="photobox">
         <ul className={cx('wrap-images')}>
           {images.map((imgSrc, idx) => (
-            <li key={idx} className={cx('wrap-image')}>
+            <li
+              key={idx}
+              className={cx('wrap-image')}
+              onClick={() => {
+                handleSelectedImage(idx)
+              }}
+            >
               <img src={imgSrc} alt={`Gallery image ${idx + 1}`} />
             </li>
           ))}
         </ul>
       </Section>
-      <ImageViewer images={images} />
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
     </>
   )
 }
